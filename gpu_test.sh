58a950f2ed26c7407791bc489694595bdf3bca02
Cheney <myuaj@connect.ust.hk>
2018-06-04 15:47:27 +0800

    update script

--- gpu_test.sh
+++ gpu_test.sh
@@ -6,14 +6,15 @@ main() {
 
     cd /home/ubuntu/example
 
-    mkdir -p log
+    LOG_DIR=/home/ubuntu/logs
+    mkdir -p ${LOG_DIR}
     NUM=8216
     BATCH=1
 
     while [ $BATCH -le 256 ]; do
         for CUR in 10 20; do 
             ./countable_client/inception_client --server=0.0.0.0:9000 --image=test.jpg --concurrency=${CUR} --batch_size=${BATCH} --num_tests=${NUM} \
-            --time_out=5 &> log/${CUR}-${BATCH}.log &
+            --time_out=5 &> ${LOG_DIR}/client/${CUR}-${BATCH}.log &
 
             PID=$!
             wait $PID
